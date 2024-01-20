b694b354078d58513898fefc76f69b9c594deb17
trapbed <148848875+trapbed@users.noreply.github.com>
2024-01-20 13:05:34 +0500

    time and date

--- index.php
+++ index.php
@@ -7,6 +7,53 @@
 </head>
 <body>
     <h1>Задачник ЕГЭ по информатике.</h1>
+    <span id='time'></span>
+    <?php
+                    switch (date("m")){
+                        case 1:
+                            $month= "Январь";
+                            break;
+                        case 2:
+                            $month= "Февраль";
+                            break;
+                        case 3:
+                            $month= "Март";
+                            break;
+                        case 4:
+                            $month= "Апрель";
+                            break;
+                        case 5:
+                            $month= "Май";
+                            break;
+                        case 6:
+                            $month= "Июнь";
+                            break;
+                        case 7:
+                            $month= "Июль";
+                            break;
+                        case 8:
+                            $month= "Август";
+                            break;
+                        case 9:
+                            $month= "Сентябрь";
+                            break;
+                        case 10:
+                            $month= "Октябрь";
+                            break;
+                        case 11:
+                            $month= "Ноябрь";
+                            break;
+                        case 12:
+                            $month= "Декабрь";
+                            break;
+                        }
+                        $hour=date('H');
+                        $minutes=date('i');
+                        $seconds=date('s');
+                        
+                        $clock=[$hour, $minutes, $seconds];
+                        echo date("$month d, 20y "." ".$clock[0].":".$clock[1].":"."$clock[2]");
+                        ?>
     <nav>
         <li><a href="/task.php?task=0">1.Задания на перевод из 2СС в 10СС.</a></li>
         <li><a href="/task.php?task=1">2.Задания на получение размера картинки в Кбит.</a></li>
