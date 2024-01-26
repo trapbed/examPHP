511c865d15c86d222d4ff73ae38bc8ee068f08e6
trapbed <148848875+trapbed@users.noreply.github.com>
2024-01-26 14:24:36 +0500

    update timer

--- time.js
+++ time.js
@@ -10,7 +10,16 @@
         countdownInterval = setInterval(function() { 
         date= new Date();
 
-        time.textContent = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+    let hour=date.getHours();
+    if (hour<10) {hour=`0${hour}`;}
+
+    let min=date.getMinutes();
+    if (min<10) {min=`0${min}`;}
+
+    let sec=date.getSeconds();
+    if (sec<10) {sec=`0${sec}`;}
+    
+        time.textContent = hour+":"+min+":"+sec;
         }, 1000);
     }
     startCountdown();    
